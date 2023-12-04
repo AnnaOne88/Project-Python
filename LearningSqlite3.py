20c3407dfb050b89896b11537abf40f94b3789a4
AnnaOne88 <137058268+AnnaOne88@users.noreply.github.com>
2023-12-04 17:34:51 +0100

    Update LearningSqlite3.py

--- LearningSqlite3.py
+++ LearningSqlite3.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import random
 
 def create_database_with_data():
     # Connect to SQLite database (or create if it doesn't exist)
@@ -17,19 +17,39 @@ def create_database_with_data():
         )
     ''')
 
-    # Insert some sample data into the table
-    sample_data = [
+    # Insert some sample data into the table THE FIRST TIME YOU RUN THE CODE, UNCOMMENT THIS TO ADD ENTRIES. THE SECOND TIME YOU RUN IT, THIS NEEDS TO BE DEACTIVATED.
+    '''sample_data = [
         ('hello', '你好'),
         ('world', '世界'),
         ('english word', 'chinese word'),
     ]
 
-    cursor.executemany('INSERT INTO vocabulary (english_word, chinese_word) VALUES (?, ?)', sample_data)
+    cursor.executemany('INSERT INTO vocabulary (english_word, chinese_word) VALUES (?, ?)', sample_data)'''
 
     # Commit the changes and close the connection
     conn.commit()
     conn.close()
 
+def print_database():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('vocabulary.db')
+
+    # Create a cursor object to execute SQL statements
+    cursor = conn.cursor()
+
+    # Print the entire vocabulary table
+    cursor.execute('SELECT * FROM vocabulary')
+    rows = cursor.fetchall()
+
+    if not rows:
+        print("No entries found in the vocabulary table.")
+    else:
+        print("Entries in the vocabulary table:")
+        for row in rows:
+            print(row)
+
+    # Close the connection
+    conn.close()
 
 def select_random_entry():
     # Connect to the SQLite database
@@ -38,11 +58,14 @@ def select_random_entry():
     # Create a cursor object to execute SQL statements
     cursor = conn.cursor()
 
-    # Use SELECT with ORDER BY RANDOM() to get a random entry
+    # Use SELECT with ORDER BY RANDOM() in a subquery to get a random entry
     cursor.execute('''
         SELECT * FROM vocabulary
-        ORDER BY RANDOM()
-        LIMIT 1
+        WHERE id = (
+            SELECT id FROM vocabulary
+            ORDER BY RANDOM()
+            LIMIT 1
+        )
     ''')
 
     # Fetch the result
@@ -57,9 +80,27 @@ def select_random_entry():
     # Close the connection
     conn.close()
 
+def delete_extra_rows():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('vocabulary.db')
+
+    # Create a cursor object to execute SQL statements
+    cursor = conn.cursor()
+
+    # Delete all rows except for the first three
+    cursor.execute('DELETE FROM vocabulary WHERE id > 3')
+
+    # Commit the changes
+    conn.commit()
+
+    # Close the connection
+    conn.close()
 
 if __name__ == "__main__":
     create_database_with_data()
-    print("Database and table created with sample data.")
+    print_database()
 
+    delete_extra_rows()
+
+    print()
     select_random_entry()
