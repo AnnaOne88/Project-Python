a2aebafd0a63673a5fb34289e0cf9509f990854a
AnnaOne88 <137058268+AnnaOne88@users.noreply.github.com>
2023-12-05 21:22:23 +0100

    Update database with our XLS vocabulary file.

--- SQLITE3_AddData.py
+++ SQLITE3_AddData.py
@@ -1,29 +1,54 @@
 import sqlite3
+import openpyxl
 
-def add_data():
+#this adds data from our external XLS file. RUN ONLY ONCE.
+
+def read_and_insert_data(xlsx_file_path):
+    # Load the workbook using openpyxl
+    wb = openpyxl.load_workbook(xlsx_file_path)
+    sheet = wb.active
+    word_dictionary = {}
+
     # Connect to the SQLite database
     conn = sqlite3.connect('vocabulary.db')
 
     # Create a cursor object to execute SQL statements
     cursor = conn.cursor()
 
-    # Insert data into the 'vocabulary' table
-    data_to_insert = [
-        ('apple', '苹果'),
-        ('book', '书'),
-        ('cat', '猫'),
-        # Add more data as needed
-    ]
+    # Iterate through rows in the sheet and insert data into the 'vocabulary' table. This structure from our other script... Debugging prints added.
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        english_word, chinese_words_str = row
 
-    cursor.executemany('INSERT INTO vocabulary (english_word, chinese_word) VALUES (?, ?)', data_to_insert)
+        # Skip the row if english_word is None
+        if english_word is None:
+            print("Skipping row with None for english_word.")
+            continue
 
-    # Commit the changes
+        # Add a check for None before splitting
+        if chinese_words_str is not None:
+            chinese_words = [word.strip() for word in chinese_words_str.split(',')]
+        else:
+            chinese_words = []
+
+        # Add debug prints
+        print(f"Inserting: english_word={english_word}, chinese_words={chinese_words}")
+
+        # Insert data into the 'vocabulary' table
+        cursor.execute('INSERT INTO vocabulary (english_word, chinese_word) VALUES (?, ?)',
+                       (english_word, chinese_words_str))
+
+        # Commit the changes
     conn.commit()
 
-    # Close the connection
+        # Close the connection
     conn.close()
+    return word_dictionary
 
-'''def delete_extra_rows():     #this is here just in case you need it after running the script more times, like me!!!!!!
+if __name__ == "__main__":
+    xlsx_file_path = './vocabulary-list-extended.xlsx'  # Replace with the actual path to your XLSX file
+    word_dict = read_and_insert_data(xlsx_file_path)
+
+'''def delete_extra_rows():     # this is here just in case you need it after running the script more times, like me!!!!!!
     # Connect to the SQLite database
     conn = sqlite3.connect('vocabulary.db')
 
@@ -60,38 +85,6 @@ def print_database():
     # Close the connection
     conn.close()
 
-def select_random_entry():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('vocabulary.db')
-
-    # Create a cursor object to execute SQL statements
-    cursor = conn.cursor()
-
-    # Use SELECT with ORDER BY RANDOM() in a subquery to get a random entry
-    cursor.execute('''
-        SELECT * FROM vocabulary
-        WHERE id = (
-            SELECT id FROM vocabulary
-            ORDER BY RANDOM()
-            LIMIT 1
-        )
-    ''')
-
-    # Fetch the result
-    random_entry = cursor.fetchone()
-
-    # Print the result
-    if random_entry:
-        print("Random entry:", random_entry)
-    else:
-        print("No entries found in the vocabulary table.")
-
-    # Close the connection
-    conn.close()
-
 if __name__ == "__main__":
-    add_data()
     print_database()
-    print()
-    select_random_entry()
     #delete_extra_rows()
